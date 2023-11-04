0e4d98e050f4a8e679ae7b089f0383274b0cb83e
agung <agungsdas@gmail.com>
2023-11-04 19:50:49 +0700

    feat: first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+require('dotenv').config()
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec');
+const { execSync } = require('child_process');
+const dayjs = require('dayjs');
+
+const run = async () => {
+  const targetRepository = core.getInput('target-repository');
+  const authorName = core.getInput('author-name');
+  const authorEmail = core.getInput('author-email');
+  const commitMessage = core.getInput('commit-message') || `Auto Commit by ${authorName} (${authorEmail}) at ${dayjs().format('DD/MM/YYYY HH:mm:ss Z')}`;
+  const script = core.getInput('script');
+
+  if (!targetRepository) {
+    throw new Error('Target repository not found')
+  }
+  if (!authorName) {
+    throw new Error('Author Name not found')
+  }
+  if (!authorEmail) {
+    throw new Error('Author Email not found')
+  }
+
+  await exec.exec(`git clone ${targetRepository} repo`)
+  await exec.exec(`git config user.name ${authorName}`, [], {
+    cwd: 'repo'
+  })
+  await exec.exec(`git config user.email ${authorEmail}`, [], {
+    cwd: 'repo'
+  })
+
+  if (script) {
+    execSync(script, {
+      cwd: 'repo',
+    })
+  }
+
+  await exec.exec('git add .', [], {
+    cwd: 'repo'
+  })
+  await exec.exec(`git commit -m "${commitMessage}"`, [], {
+    cwd: 'repo'
+  })
+  await exec.exec('git push', [], {
+    cwd: 'repo'
+  })
+}
+
+
+run().catch(error => {
+  core.setFailed(error.message);
+});
